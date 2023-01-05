60b9bd8b691cf9f3f6bc1bfb3d9564ce9132a57c
jun_neng <soojunneng01@gmail.com>
2023-01-05 21:55:09 +0800

    first commit

--- bot.py
+++ bot.py
@@ -78,11 +78,11 @@ def main():
     dp.add_handler(MessageHandler(Filters.attachment, downloader))
     dp.add_handler(CommandHandler("start", start_command))
     dp.add_error_handler(error)
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=int(PORT),
-    #                       url_path=API_KEY)
-    # updater.bot.setWebhook('https://shrouded-eyrie-14916.herokuapp.com/' + "API_KEY")
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=API_KEY)
+    updater.bot.setWebhook('https://shrouded-eyrie-14916.herokuapp.com/' + API_KEY)
+    # updater.start_polling()
     updater.idle()
 
 
